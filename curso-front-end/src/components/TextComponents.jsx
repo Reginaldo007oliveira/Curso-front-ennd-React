669d843aa21f23bcf462ef2860fd4a395fe403ae
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-12 08:05:31 -0300

    Uso do fragment

--- curso-front-end/src/components/TextComponents.jsx
+++ curso-front-end/src/components/TextComponents.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const TextComponents = ({text, description}) => {
     return (
-        <div>
+        <>
             <p><h1>{text}</h1></p>
             <p><h1>{description}</h1></p>
-        </div>
+        </>
     );
 };
 
