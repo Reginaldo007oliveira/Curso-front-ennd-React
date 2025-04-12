91715ec6b0e45ef27ab50cdc78e1de52047425d1
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-12 06:41:17 -0300

    Reuso da função Other components

---
+++ curso-front-end/src/components/OtherComponets.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ export const SecondText = () => {
+    return (
+        <div>
+            bora bora
+        </div>
+    );
+};
+export const ThirdText = () => {
+    return (
+        <div><h1> bora bora</h1>
+           
+        </div>
+    );
+};
+
---
+++ curso-front-end/src/components/TextComponents.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TextComponents = () => {
+    return (
+        <div>
+            <p>Olá</p>
+        </div>
+    );
+};
+
+export default TextComponents;
