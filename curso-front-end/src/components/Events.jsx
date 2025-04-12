d93a8cbb21bd3676f8f603085e90e9bba9092e3a
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-12 08:50:30 -0300

    Evento alert chamando da função

--- curso-front-end/src/components/Events.jsx
+++ curso-front-end/src/components/Events.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 //passar uma hero function para corrigir o problema do clique ()=>
 export const Events = () => {
+    const handleGreetings = (nome) =>{
+        alert(`Olá ${nome}`);
+    }
   return (
     <>
+    
 
 {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
 {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
 <button  onClick={()=>alert("Clicado")}>Alerta</button>
+{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+<button  onClick={()=> handleGreetings("João")}>Alert chamado da função</button>
     </>
   );
 };
