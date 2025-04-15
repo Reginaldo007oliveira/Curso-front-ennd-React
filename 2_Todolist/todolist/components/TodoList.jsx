48ab8cd78d9502a1079761429d9f74802aa14c7e
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-15 08:26:16 -0300

    TodoListCss

--- 2_Todolist/todolist/components/TodoList.jsx
+++ 2_Todolist/todolist/components/TodoList.jsx
@@ -5,21 +5,21 @@ import styles from "./TodoList.module.css";
 
 //função do campó imput
 export const TodoList = () => {
-    const [task, setTask] = useState("")
-    // console.log("task", task)
+  const [task, setTask] = useState("");
+  // console.log("task", task)
 
-    //add task
-    const [tasks, setTasks] = useState([])
+  //add task
+  const [tasks, setTasks] = useState([]);
 
-    //função do botão
-    const addTask = () => {
-        //limpa dados do formulário
-        if(task.trim() ==="") return;
-        setTasks([...tasks, task])
-        setTask("");
-    }
-    console.log("adiciona Task", tasks)
-    
+  //função do botão
+  const addTask = () => {
+    //limpa dados do formulário
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+  console.log("adiciona Task", tasks);
+
   return (
     <div className={styles.container}>
       <h2 className={styles.tile}> Lista de Tarefas</h2>
@@ -33,10 +33,18 @@ export const TodoList = () => {
           onChange={(e) => setTask(e.target.value)} //onChange quando eu teclar..  é o valor atual do campo de entrada.
         />
         {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-        <button onClick={addTask} className={styles.button}>add</button>
+        <button onClick={addTask} className={styles.button}>
+          add
+        </button>
       </div>
       <ul className={styles.TaskList}>
-        <li className={styles.taskItem}>Item 1</li>
+
+        {tasks.map((taskItem, index) => (
+          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+<li key={index} className={styles.taskItem}>{taskItem} </li>
+       
+       ))}
+
       </ul>
     </div>
   );
