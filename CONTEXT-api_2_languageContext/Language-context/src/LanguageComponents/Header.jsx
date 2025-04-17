def60f681a7918415cd9509ef2fb75c309436155
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-04-17 17:06:23 -0300

    updates

---
+++ CONTEXT-api_2_languageContext/Language-context/src/LanguageComponents/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useLanguage } from "./LanguageContext";
+
+export const Header = () => {
+  const { language, switchLanguage } = useLanguage();
+  return (
+    <header>
+      <h1>{language === "pt" ? "Portugues" : "Ingles..."}</h1>
+      {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+      <button onClick={switchLanguage}>
+        {language === "pt" ? "Mudar para Ingles" : "kkk Portugues"}
+      </button>
+    </header>
+  );
+};
